8e4105a1a9ca228c4181e323c70a6fbcce812193
Jérémie Bousquet <bousquet@lpl.dom>
2019-02-26 18:43:16 +0100

    config for other computer

--- src/config.py
+++ src/config.py
@@ -7,29 +7,28 @@ import random
 import numpy as np
 from datetime import datetime
 
-ROOT_PATH = os.path.join('C:\\', 'IAAA' ,'TER')
+ROOT_PATH = os.path.join('C:\\', 'Users', 'bousquet', 'DEV', 'TER')
 
 # Corpus
-
 CORPUS_PATH = os.path.join(ROOT_PATH, 'data', 'ACORFORMED', 'Data') # profBCorpusPath
 PREV_CORPUS_PATH = os.path.join(ROOT_PATH, 'data', 'CorpusHMPassation') # gregCorpusPath
 
 # all produced files shall go here
 OUT_PATH = os.path.join(ROOT_PATH, '_output')
 LOG_PATH = os.path.join(OUT_PATH, 'logs')
-TMP_PATH = os.path.join(ROOT_PATH, 'tmp')
+TMP_PATH = os.path.join(OUT_PATH, 'tmp')
 
 # Tools
 
-TOOLS_PATH = os.path.join(ROOT_PATH, 'resources', 'tools')
+TOOLS_PATH = os.path.join('C:\\', 'Users', 'bousquet', 'TOOLS')
 
 #  SPPAS
 
 SPPAS_1 = 'SPPAS-1.8.6'
-SPPAS_2 = 'SPPAS-2.0-2019-01-08'
+SPPAS_2 = 'SPPAS'
 SPPAS_PATH = os.path.join(TOOLS_PATH, SPPAS_1)
 SPPAS_SRC_PATH = os.path.join(SPPAS_PATH, 'sppas', 'src')
-SPPAS_2_PATH = os.path.join(TOOLS_PATH, SPPAS_2)
+SPPAS_2_PATH = os.path.join('C:\\', SPPAS_2)
 SPPAS_2_SRC_PATH = os.path.join(SPPAS_2_PATH, 'sppas', 'src')
 
 SPPAS_GREG_SRC_PATH = os.path.join(PREV_CORPUS_PATH, 'tools')
