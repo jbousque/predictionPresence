51066c4434fad15fe94c9c984f9f099478fb4d01
Jeremie Bousquet <jeremie.bousquet@gmail.com>
2019-06-18 16:24:29 +0200

    TU for compute MRRR

--- src/test_FEUtils.py
+++ src/test_FEUtils.py
@@ -5,8 +5,15 @@ import numpy as np
 class TestFEUtils(TestCase):
     def test_compute_mrr(self):
         feu = FEUtils()
-        y_true = np.array([0,1,1,0,0,1])
-        y_pred = np.array([[0,666], [1,666], [0,666], [1,0], [0,666], [0,1]])
+        y_true = np.array([0,1])
+        y_pred = np.array([[0,666], [1,666]])
+        mrr = feu.compute_mrr(y_true, y_pred, [0,1])
+        print(mrr)
+        assert mrr == 1
+
+        feu = FEUtils()
+        y_true = np.array([0,1])
+        y_pred = np.array([[1,0], [1,666]])
         mrr = feu.compute_mrr(y_true, y_pred, [0,1])
         print(mrr)
         assert mrr == 0.75
